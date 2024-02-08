48bbe271ac6e3e324eb7245577e8fcaa2c7c2ff6
Aashish Kumar Ray <2002raysi@gmail.com>
2024-02-08 11:20:26 +0530

    R-Programming

--- Vector.R
+++ Vector.R
@@ -2,11 +2,11 @@ library(ggplot)
 data = c(seq(from=0, to=100, by=6))
 quantile(data)
 
-Line plot 
+# Line plot 
 line(data)
 #Histogram plot
 hist(data)
 #box plot
 boxplot(data)
-bar plot
+# bar plot
 barplot(data)
